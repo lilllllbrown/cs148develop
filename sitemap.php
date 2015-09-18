9ea425859e9ba47a02e51f2fe63702a06ff3a094
lilllllbrown <lilybrown@ip0af51ff4.int.uvm.edu>
2015-09-18 12:14:14 -0400

    commit 1

---
+++ sitemap.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>CS 148 Database Design</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Lily Brown">
+        <meta name="description" content="This is a site map to the assignments for CS 148.">
+
+
+        <link rel="stylesheet"
+              href="style.css"
+              media="screen">
+
+    </head>
+
+    <body id="home">
+
+        <header>
+          <!--  <img src="swingingdog.jpg"alt="Cool Dog"> -->
+            <h1>Lily Brown: CS 148 Database Design</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><b>Assignment 1.0:</b>  <a href="assignment1.0/tables.php"> Sample Tables </a>
+                   
+                </li>
+                <li><b>Assignment 2.0:</b> <a href="assignment1.0/select.php"> SQL - Select </a>
+                   
+                </li>
+            </ul>
+        </nav>
+    </body>
